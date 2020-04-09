006358414d2fffc1c5af3b5252ff2977409df090
stecrotti <ruggentipini@gmail.com>
2020-04-09 10:47:46 +0200

    testing script

--- minsum.jl
+++ minsum.jl
@@ -8,9 +8,6 @@ function msiter!(FG::FactorGraph)
             # Update message
             aux = vcat(FG.J[f], [FG.b[vprime]-FG.mfv[f][vprime_idx]
                 for (vprime_idx,vprime) in enumerate(FG.Fneigs[f]) if vprime_idx != v_idx])
-            # aux[1] = FG.J[f]
-            # aux[2:end] = [FG.b[vprime]-FG.mfv[f][vprime_idx] for (vprime_idx,vprime) in
-            #     enumerate(FG.Fneigs[f]) if vprime_idx != v_idx]
             # Apply formula
             FG.mfv[f][v_idx] = prod(sign, aux)*minimum(abs, aux)
             # Update belief after updating the message
@@ -35,7 +32,7 @@ function ms!(FG::FactorGraph; maxiter=Int(1e3), nmin=50,
         if newguesses == oldguesses
             n += 1
             n >= nmin && return :converged, it
-        elseif energy(FG,newguesses)==0.0
+        elseif energy(FG,newguesses) == 0.0
             return :converged, it
         else
             n=0
--- test/energy.jl
+++ test/energy.jl
@@ -1,33 +1,38 @@
 include("../headers.jl")
-using PyPlot
+using UnicodePlots
 
-γ_interval = LinRange(0.0,1.0,11)
-N_values = Int.([1e3, 1e4])
-p=3
-N_avg = 10
+γvals = LinRange(0.0,1.0,21)
+nvals = Int.([1e2, 1e3, 1e4])
+p = 3
+navg = 200
+k = 1e-3
 
-E = [zeros(length(γ_interval)) for N in N_values]
+E = [zeros(length(γvals)) for _ in eachindex(nvals)]
 
-for (i,N) in enumerate(N_values)
-    for (j,γ) in enumerate(γ_interval)
-        for n in 1:N_avg
-            FG = pspingraph(N, trunc(Int, γ*N),p)
-            ms!(FG, maxiter=1e3, nmin=30)
+for (i,n) in enumerate(nvals)
+    for (j,γ) in enumerate(γvals)
+        for _ in 1:navg
+            FG = pspingraph(n, round(Int, γ*n), p)
+            ms!(FG, maxiter=1e3, nmin=10, k=1e-3)
             E[i][j] += energy(FG)
         end
         println("γ=$(round(γ,digits=2)) completed")
-        E[i][j] /= N_avg
+        E[i][j] /= navg*n
     end
-    println("  N=$(N) completed")
+    println("  n=$(n) completed")
 end
 
-for (i,N) in enumerate(N_values)
-    plot(γ_interval,E[i]./N,"-o", markersize=5)
+for (i,n) in enumerate(nvals)
+    if i == 1
+        global plt = lineplot(γvals, E[i],
+        title = "GS energy density",
+        name = "n="*string(nvals[i]),
+        xlabel = "γ", canvas = DotCanvas)
+    end
+    if i > 1
+        lineplot!(plt, γvals, E[i],
+        name = "n="*string(nvals[i]))
+    end
 end
-
-grid();
-plt.:xlabel("γ")
-plt.:ylabel("Energy / N")
-plt.:xlim((-0.05, 1.05))
-plt.:title("GS energy density")
-plt.:legend(["N="*string(N) for N in N_values]);
+print("\a")
+plt
