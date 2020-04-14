e97f56f3f5fcd24fa3a142a132124b7681e9702e
stecrotti <ruggentipini@gmail.com>
2020-04-14 15:07:17 +0200

    small adjustments

--- test/energy.jl
+++ test/energy.jl
@@ -1,14 +1,13 @@
 include("../headers.jl")
-# using UnicodePlots
+using UnicodePlots
 using PyPlot
 
-# γvals = LinRange(0.0,1.0,21)
-γvals = LinRange(0.0,1.0,3)
-# nvals = Int.([1e2, 1e3, 1e4])
-nvals = [10,20]
+γvals = LinRange(0.6,1.0,12)
+nvals = Int.([1e2, 1e3, 1e4])
+# nvals = Int.([1e2, 1e3])
 p = 3
-# navg = 200
-navg = 2
+navg = 100
+# navg = 50
 k = 1e-3
 
 E = [zeros(length(γvals)) for _ in eachindex(nvals)]
@@ -26,30 +25,30 @@ for (i,n) in enumerate(nvals)
     println("  n=$(n) completed")
 end
 
-PyPlot.close("all")
-for (i,n) in enumerate(nvals)
-    plot(γvals, E[i], "o-")
-end
-
-plt.:title = "GS energy density"
-plt.:xlabel = "γ"
-plt.:ylabel = "Energy / n"
-plt.:legend("n = " .* string.(nvals))
-plt.savefig("../images/energy.png")
-
-
+# PyPlot.close("all")
 # for (i,n) in enumerate(nvals)
-#     if i == 1
-#         global myplt = lineplot(γvals, E[i],
-#         title = "GS energy density",
-#         name = "n="*string(nvals[i]),
-#         xlabel = "γ", canvas = DotCanvas)
-#     end
-#     if i > 1
-#         lineplot!(myplt, γvals, E[i],
-#         name = "n="*string(nvals[i]))
-#     end
+#     plot(γvals, E[i], "o-")
 # end
-# myplt
+#
+# plt.:title = "GS energy density"
+# plt.:xlabel = "γ"
+# plt.:ylabel = "Energy / n"
+# plt.:legend("n = " .* string.(nvals))
+# plt.savefig("../images/energy.png")
+
+
+for (i,n) in enumerate(nvals)
+    if i == 1
+        global myplt = lineplot(γvals, E[i],
+        title = "GS energy density",
+        name = "n="*string(nvals[i]),
+        xlabel = "γ", canvas = DotCanvas)
+    end
+    if i > 1
+        lineplot!(myplt, γvals, E[i],
+        name = "n="*string(nvals[i]))
+    end
+end
+myplt
 
 print("\a")
